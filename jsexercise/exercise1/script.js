e9944f38949ad5aa81cb978878b6f95edae8c380
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2022-10-17 04:42:14 +0900

    used skills

--- jsexercise/exercise1/script.js
+++ jsexercise/exercise1/script.js
@@ -2,6 +2,13 @@
 
 const numberOFSeries = +prompt("How many movies have you watched?", "");
 
+if (numberOFSeries < 5){
+    console.log("You have to watch at least 5 movies.");
+} else if( numberOFSeries >= 5 && numberOFSeries <=10){
+    console.log("You have to watch at most 10 movies.");
+} else {
+    console.log("You have to watch more than 10 movies.");
+}
 const seriesDB = {
 
     count : numberOFSeries,
@@ -12,10 +19,16 @@ const seriesDB = {
 
 };
 
-const name1 = prompt("What is the name of movie that has been watched? " ),
+let name1 = prompt("What is the name of movie that has been watched? " ),
     score1 = +prompt(" How do you like it?" , '10 out of ?'),
     name2 = prompt("What is the name of movie that has been watched?"),
     score2 = +prompt(" How do you like it?" , '10 out of ?');
+
+while( name1.length <= 0 && name2.length <= 0) {
+    name1 = prompt("What is the name of movie that has been watched? " );
+    name2 = prompt("What is the name of movie that has been watched? ");
+
+}
 
 seriesDB.series[name1] = score1;
 seriesDB.series[name2] = score2;
--- jsexercise/js/script.js
+++ jsexercise/js/script.js
@@ -14,7 +14,60 @@
 
 // document.getElementById('demo').innerHTML = `I'm ${ages}`;
 
-const channel = prompt("Who is your favourite youtuber?", "ex:ulugbeksamigjanov");
-const youtuber = `https://youtube.com/${channel}/videos`;
-document.getElementById('demo').innerHTML =`My favourite youtober is: ${youtuber}`;
-console.log(youtuber);
+// const channel = prompt("Who is your favourite youtuber?", "ex:ulugbeksamigjanov");
+// const youtuber = `https://youtube.com/${channel}/videos`;
+// document.getElementById('demo').innerHTML =`My favourite youtober is: ${youtuber}`;
+// console.log(youtuber);
+
+
+// Logical operations
+// const age = prompt("How old are you?");
+
+// if (age>18) {
+//     console.log("welcome");
+// } else {
+//     console.log("No entry");
+// }
+
+// const color = prompt("You favourite color?", '');
+// switch  (color) {
+//     case "red":
+//         console.log("red");
+//         break;
+//     case "green":
+//         console.log("green");
+//         break;
+//     case "yellow":
+//         console.log("yellow");
+//         break;
+//     default:
+//         console.log("there is no more option exsept red, green, or yellow"); 
+//         break;
+// };
+
+
+// // tsikllar
+
+// let num=1;
+// // while(num <= 10){
+// //     console.log(num);
+// //     num++;
+// // }
+
+// do {
+  
+//     console.log(num);
+//     num++;
+//     if (num ===8 ) {
+//         continue;
+//     }
+
+// } while(num <= 10);
+
+
+// for (let i=1;i<=10;i++) {
+//     if (i === 8) {
+//        break;
+//     }
+//     console.log(i);
+// }
