ce92e11f3ab5b79569da75f3f18028c5d27b4943
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2022-10-24 06:22:37 +0900

    final

--- jsexercise/js/script.js
+++ jsexercise/js/script.js
@@ -155,9 +155,9 @@
 
 // edu('JavaScript', function () { console.log("That's great")});
 
- 
+//  ---------------------------------------
 // Obyektlar destruptatziya
-
+// ----------------------------------------
 // const school = {
 //     nameOF: "genius",
 //     adress: "shakhrikhon",
@@ -173,9 +173,9 @@
 //     } 
 // };
 
-
+// ----------------------------------
 // // getting key through key method
-
+// --------------------------------
 // const informAboutSchool = function (cb) {
 //     for (let key in school) {
 //     if (typeof school[key] === "object") {
@@ -202,10 +202,18 @@
 
 // informAboutSchool(cb);
 
-
-// arrays 
-
+// -------------------------------
+// Arrays 
+// -------------------------------
 // const arr = [1,19,22,31,4,5];
+// // writing sort methods problem figuring out with   callback function wit
+// function help(a ,b) {
+//     return a-b;
+
+// }
+// arr.sort(help);
+// console.log(arr);
+
 // arr.sort((a,b) => a-b); 
 // for (let value of arr) { 
 //     console.log(value);
@@ -214,6 +222,89 @@
 // arr.forEach(function (item,index,array ){
 //     console.log(`${index}: ${item} into ${array}`);
 // });
+
+
+//  CLONE(nusxa olish), ES6
+
+// massive cloning
+
+// let x =10;
+// let y = x;
+
+// y= y +5;
+
+// console.log(x);
+// console.log(y);
+
+// object cloning
+
+// 1-way object cloning and most usefull than others ways
+
+// const numbers = {
+//     x : 5,
+//     y :10,
+// };
+// const otherNumber = {
+//     z : 6,
+// }
+// const newNumbers = Object.assign(otherNumber ,numbers); // adding two 2 object 
+// const newNumbers1 = Object.assign({},numbers); // cloning object
+
+// newNumbers.x =15;
+// console.log(newNumbers);
+
+// 2-way object cloning with using for method and some funcyion
+const numbers = {
+    x : 5,
+    y :10,
+    z : {
+        a: 8,
+        b : 3,
+    }
+};
+
+// for (let key in numbers) {
+//   if (typeof numbers[key] === 'object') {
+//     for (let i in numbers[key]) {
+//       console.log(numbers[key][i]);
+//     }
+//   } else {
+//     console.log(numbers[key]);
+//   }
+// }
+
+
+const objOf = (obj) => {
+  const objectOfCopy = {};
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      const objOfObj = (obj2) =>{
+        const objectOfCopy2 = {}; 
+        for (let i in obj2) {
+          objectOfCopy2[i] = obj2[i];
+        } 
+        objectOfCopy[key] = objectOfCopy2;
+      };
+      objOfObj(obj[key]);
+    } else {
+          objectOfCopy[key] = obj[key];
+      }
+  } 
+  return objectOfCopy;
+  
+};
+
+const newNumbers = objOf(numbers);
+newNumbers.z.a = 0;
+
+console.log(newNumbers);
+console.log(numbers);
+
+
+
+
+
+
 
 
 
